400e33449d9d475aaa0b0fec5c5b2a6b4b99ad26
bluerhodfa <ira.theobold@gmail.com>
2024-12-21 20:40:45 +0000

    fix node helper

--- node_helper.js
+++ node_helper.js
@@ -13,8 +13,29 @@ module.exports = {
     DATA: 'AIR_QUALITY_DATA',
     DATA_RESPONSE: 'AIR_QUALITY_DATA_RESPONSE',
   },
-
   start: function () {
     console.log('AirQuality helper started ...')
+  },
+  loadData: async function (payload) {
+    const self = this
+    const url = `https://${payload.config.apiBase}${payload.config.dataEndpoint}${payload.config.location}/?token=${payload.config.token}`
+    console.log(`AirQuality-Fetcher: ${url}`)
+
+    const result = await fetch(url)
+      .then(response => response.json())
+
+    self.sendSocketNotification(self.notifications.DATA_RESPONSE, {
+      payloadReturn: result,
+      status: 'OK',
+      identifier: payload.identifier,
+    })
+  },
+  socketNotificationReceived: function (notification, payload) {
+    switch (notification) {
+      case this.notifications.DATA:
+        console.log(`AirQuality-Fetcher: Loading data of ${payload.config.location} for module ${payload.identifier}`)
+        this.loadData(payload)
+        break
+    }
   },
 }

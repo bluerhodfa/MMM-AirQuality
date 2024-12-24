ffa02c356cdafe61531d0ddffb8ac3f7e87e7701
bluerhodfa <ira.theobold@gmail.com>
2024-12-24 20:02:57 +0000

    fix api link

--- MMM-AirQuality.js
+++ MMM-AirQuality.js
@@ -130,7 +130,7 @@ Module.register("MMM-AirQuality", {
   getTemplateData: function() {
     let message = ''
     if (this.config.appId === '') {
-      message = `Please set an API key for ${this.name}<br>You can get one at <a href='https://openweather..org/'>https://openweathermap.org</a>`
+      message = `Please set an API key for ${this.name}<br>You can get one at <a href='https://openweathermap.org/'>https://openweathermap.org</a>`
     } else if (this.config.location === '') {
       message = `Please set a location for ${this.name}!`
     }

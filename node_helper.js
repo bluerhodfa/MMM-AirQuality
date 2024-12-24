16eb631def6dc879c02df440a3d18b393b9cfd19
bluerhodfa <ira.theobold@gmail.com>
2024-12-24 20:53:21 +0000

    rename helper

--- node_helper.js
+++ node_helper.js
@@ -5,37 +5,7 @@
  * MIT Licensed.
  */
 
-const NodeHelper = require("node_helper")
+const NodeHelper = require('node_helper')
+const helper = require('./fetch_api_data')
 
-module.exports = {
-    notifications: {
-      DATA: "AIR_QUALITY_DATA",
-      DATA_RESPONSE: "AIR_QUALITY_DATA_RESPONSE",
-    },
-    start: function () {
-      console.log("AirQuality helper started ...")
-    },
-    loadData: async function (payload) {
-      const self = this
-      const url = `https://${payload.config.apiBase}${payload.config.apiVersion}${payload.config.apiEndPoint}?lat=${payload.config.lat}&lon=${payload.config.lon}&appid=${payload.config.appId}`
-      console.log(`AirQuality-Fetcher: ${url}`)
-  
-      const result = await fetch(url)
-        .then(response => response.json())
-        console.log(`JSON response = `+ JSON.stringify(result))
-  
-      self.sendSocketNotification(self.notifications.DATA_RESPONSE, {
-        payloadReturn: result,
-        status: "OK",
-        identifier: payload.identifier,
-      })
-    },
-    socketNotificationReceived: function (notification, payload) {
-      switch (notification) {
-        case this.notifications.DATA:
-          console.log(`AirQuality-Fetcher: Loading AQI data  for location ${payload.config.lat}, ${payload.config.lon} for module ${payload.identifier}`)
-          this.loadData(payload)
-          break
-      }
-    },
-  }
+module.exports = NodeHelper.create(helper)

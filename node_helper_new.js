a8e43b1066dc9c4713a1582d7c7f7ab2195f563e
bluerhodfa <ira.theobold@gmail.com>
2024-12-24 15:04:25 +0000

    revert to helper.js

---
+++ node_helper_new.js
@@ -0,0 +1,39 @@
+/* MagicMirror²
+ * Module: MMM-AirQuality
+ *
+ * By Ira theobold https://github.com/bluerhodfa
+ * MIT Licensed.
+ */
+const NodeHelper = require('node_helper')
+
+module.exports = {
+    notifications: {
+      DATA: 'AIR_QUALITY_DATA',
+      DATA_RESPONSE: 'AIR_QUALITY_DATA_RESPONSE',
+    },
+    start: function () {
+      console.log('AirQuality helper started ...')
+    },
+    loadData: async function (payload) {
+      const self = this
+      const url = `https://${payload.config.apiBase}${payload.config.apiVersion}${payload.config.apiEndPoint}?lat=${payload.config.lat}&lon=${payload.config.lon}&appid=${payload.config.appId}`
+      console.log(`AirQuality-Fetcher: ${url}`)
+  
+      const result = await fetch(url)
+        .then(response => response.json())
+  
+      self.sendSocketNotification(self.notifications.DATA_RESPONSE, {
+        payloadReturn: result,
+        status: 'OK',
+        identifier: payload.identifier,
+      })
+    },
+    socketNotificationReceived: function (notification, payload) {
+      switch (notification) {
+        case this.notifications.DATA:
+          console.log(`AirQuality-Fetcher: Loading AQI data  for location ${payload.config.lat}, ${payload.config.lon} for module ${payload.identifier}`)
+          this.loadData(payload)
+          break
+      }
+    },
+  }

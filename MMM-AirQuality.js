a33de49eb29561561e156961a158fd762b7acb48
bluerhodfa <ira.theobold@gmail.com>
2024-12-24 15:14:57 +0000

    fix quotes in nerror message

--- MMM-AirQuality.js
+++ MMM-AirQuality.js
@@ -126,7 +126,7 @@ Module.register("MMM-AirQuality", {
   getTemplateData: function() {
     let message = ''
     if (this.config.appId === '') {
-      message = `Please set an API key for ${this.name}<br>You can get one at <a href='https://api.openweather.org/`>https://api.openweathermap.org</a>`
+      message = `Please set an API key for ${this.name}<br>You can get one at <a href='https://api.openweather.org/'>https://api.openweathermap.org</a>`
     } else if (this.config.location ==='') {
       message = `Please set a location for ${this.name}!`
     }
